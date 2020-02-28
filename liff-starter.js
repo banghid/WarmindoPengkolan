9902340805be41a3389b0e596787d918fef7d57f
banghid <wahidrochman@gmail.com>
2020-02-29 02:30:44 +0700

    add liff starter

---
+++ liff-starter.js
@@ -0,0 +1,136 @@
+window.onload = function(){
+	const useNodeJs = false;
+	const defaultLiffId = "1653765752-6X1g1ge2";
+
+	let myLiffId = "";
+
+	if (useNodeJs) {
+		fetch('/send-id')
+			.then(function(reqResponse){
+				return reqResponse.json();
+			})
+			.then(function(jsonResponse){
+				myLiffId = jsonResponse.id;
+				initializeLiffOrDie(myLiffId);
+			})
+			.catch(function(error){
+				document.getElementById("appContent").classList.add('hidden');
+				document.getElementById("nodeLiffIdErrorMessage").classList.remove('hidden');
+			});
+	} else{
+		myLiffId = defaultLiffId;
+		initializeLiffOrDie(myLiffId);
+	}
+};
+
+function initializeLiffOrDie(myLiffId){
+	if (!myLiffId) {
+		document.getElementById("appContent").classList.add('hidden');
+		document.getElementById("liffIdErrorMessage").classList.remove('hidden');
+	} else {
+		initializeLiff(myLiffId);
+	}
+}
+
+function initializeLiff(myLiffId){
+	liff
+		.init({
+			liffId: myLiffId
+		})
+		.then(() => {
+			initializeApp();
+		})
+		.catch((err) => {
+			document.getElementById("appContent").classList.add('hidden');
+			document.getElementById("liffInitErrorMessage").classList.remove('hidden');
+		});
+}
+
+function initializeApp(){
+	displayLiffData();
+	displayIsInClientInfo();
+	registerButtonHandlers();
+
+	if (liff.isLoggedIn()) {
+		document.getElementById('btnLogin').disabled = true;
+		var text = "Selamat datang "+ +" di Warmindo Pengkolan !!!"
+		document.getElementById('welcomeStatement').textContent = text;
+	} else {
+		document.getElementById('btnLogout').disabled = true;
+		document.getElementById('welcomeStatement').textContent = 'Anda belum Login, login lah dengan akun LINE anda terlebih dahulu !!';
+	}
+}
+
+function displayLiffData(){
+	document.getElementById('isInClient').textContent = liff.isInClient();
+	document.getElementById('isLoggedIn').textContent = liff.isLoggedIn();
+}
+
+function displayIsInClientInfo(){
+	if (liff.isInClient()) {
+		document.getElementById('btnLogin').classList.toggle('hidden');
+		document.getElementById('btnLogout').classList.toggle('hidden');
+		var text = "Selamat datang "+ +" di Warmindo Pengkolan !!!"
+		document.getElementById('welcomeStatement').textContent = text;
+	} else{
+		document.getElementById('welcomeStatement').textContent = 'Anda belum Login, login lah dengan akun LINE anda terlebih dahulu !!';
+	}
+}
+
+function registerButtonHandlers(){
+	document.getElementById('openWindowButton').addEventListener('click', function(){
+		liff.openWindow({
+			url: 'https://study.banghid.com/warmindo/',
+			external: true
+		});
+	});
+
+	document.getElementById('closeWindowButton').addEventListener('click', function(){
+		if (!liff.isInClient()) {
+			sendAlertIfNotInClient();
+		} else {
+			liff.closeWindow();
+		}
+	});
+
+	document.getElementById('btnLogin').addEventListener('click', function(){
+		if (!liff.isLoggedIn()) {
+			liff.login();
+		}
+	});
+
+	document.getElementById('btnLogout').addEventListener('click', function(){
+		if (liff.isLoggedIn()) {
+			liff.logout();
+			window.location.reload();
+		}
+	});
+
+	document.getElementById('btnPesan').addEventListener('click', function(){
+		if (!liff.isInClient()) {
+			sendAlertIfNotInClient();
+		} else {
+			liff.sendMessages([{
+				'type' : 'text',
+				'text' : "Anda telah menggunakan fitur Send Message!"
+			}]).then(function(){
+				window.alert('Ini adalah pesan dari fitur Send Message');
+			}).catch(function(error){
+				window.alert('Error sending message: '+ error);
+			});
+		}
+	});
+}
+
+function sendAlertIfNotInClient(){
+	alert('This button is unavailable as LIFF is currently being opened in an external browser.');
+}
+
+function toggleElement(elementId){
+	const elem = document.getElementById(elementId);
+	if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+		elem.style.display = 'none';
+	} else {
+		elem.style.display = 'block';
+	}
+}
